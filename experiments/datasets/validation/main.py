a9f5eaa8c81c76df67e0468cd894ec5c523e292d
Swarnim Burnwal <98446769+SLAYERDAKU@users.noreply.github.com>
2023-12-26 18:44:24 +0530

    api integrated

--- experiments/datasets/validation/main.py
+++ experiments/datasets/validation/main.py
@@ -6,7 +6,7 @@ import json
 import os
 from jsonschema import validate, ValidationError, SchemaError
 import gradio as gr
-
+import google.generativeai as genai
 from dotenv import load_dotenv
 load_dotenv(".env")
 
@@ -19,8 +19,45 @@ def save_to_database(data):
     db = client["function_calling"]
 
 
-def echo(message, history):
-    return message
+def gemini_api(prompt: str, history):
+    """
+    This function is used to call the Gemini API.
+    """
+    genai.configure(api_key="AIzaSyBaYulzAX6zbLPZSbzU2CPBpb_lvoQ9x_Q")
+    generation_config = {
+        "temperature": 0.9,
+        "top_p": 1,
+        "top_k": 1,
+        "max_output_tokens": 2048,
+    }
+    safety_settings = [
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+    ]
+    model = genai.GenerativeModel(model_name="gemini-pro",
+                                  generation_config=generation_config,
+                                  safety_settings=safety_settings)
+
+    prompt_parts = [
+        f"{prompt}"
+    ]
+
+    response = model.generate_content(prompt_parts)
+    return response.text
 
 
 bot1 = gr.Chatbot(height=600)
@@ -34,11 +71,10 @@ with gr.Blocks(css="footer {visibility: hidden}") as demo:
 
     with gr.Row():
         with gr.Column():
-            gr.ChatInterface(chatbot=bot1, fn=echo, title="Function Calling")
-            # create a Test Case with symbol and input
-
+            gr.ChatInterface(chatbot=bot1, fn=gemini_api,
+                             title="Function Calling")
         with gr.Column():
-            gr.ChatInterface(chatbot=bot2, fn=echo, title="Response")
+            gr.ChatInterface(chatbot=bot2, fn=gemini_api, title="Response")
 
     gr.Button("Save to Database")
 
