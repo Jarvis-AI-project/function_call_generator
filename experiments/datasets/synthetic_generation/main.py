7e40598db59dfc6cece49d5a39d6d0e118c276f8
Devasheesh Mishra <devasheeshmishra4@gmail.com>
2023-12-26 15:16:46 +0000

    Update gemini-pro model configuration

--- experiments/datasets/synthetic_generation/main.py
+++ experiments/datasets/synthetic_generation/main.py
@@ -4,13 +4,11 @@ This WebUI is used to built so that humans and AI can collaborate to generate sy
 import os
 import pymongo
 import gradio as gr
-
-import google.generativeai as genai
-genai.configure(api_key=os.environ["GOOGLE_MAKERSUITE_API_KEY"])
-
 from dotenv import load_dotenv
-load_dotenv(".env")
+import google.generativeai as genai
 
+load_dotenv(".env")
+genai.configure(api_key=os.environ["GOOGLE_MAKERSUITE_API_KEY"])
 
 _URI = f"mongodb://{os.environ['MONGODB_USERNAME']}:{os.environ['MONGODB_PASSWORD']}@{os.environ['MONGODB_HOST']}:{os.environ['MONGODB_PORT']}/{os.environ['MONGODB_DATABASE']}"
 
@@ -25,55 +23,41 @@ def gemini_api(prompt: str, history):
         "top_k": 1,
         "max_output_tokens": 2048,
     }
-    safety_settings = [
-        {
-            "category": "HARM_CATEGORY_HARASSMENT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-        {
-            "category": "HARM_CATEGORY_HATE_SPEECH",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-        {
-            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-        {
-            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-    ]
-    model = genai.GenerativeModel(model_name="gemini-pro",
-                                  generation_config=generation_config,
-                                  safety_settings=safety_settings)
+    
+    model = genai.GenerativeModel(
+        model_name="gemini-pro",
+        generation_config=generation_config
+    )
+    history.append(prompt)
 
-    prompt_parts = [
-        f"{prompt}"
-    ]
-
-    response = model.generate_content(prompt_parts)
+    response = model.generate_content(history)
     return response.text
-
 
 bot1 = gr.Chatbot(height=600)
 bot2 = gr.Chatbot(height=600)
 
-
-with gr.Blocks(css="footer {visibility: hidden}") as demo:
+with gr.Blocks(css="footer {visibility: hidden}") as iface:
     gr.HTML("""
     <h1 style="text-align: center; user-select:None; ">DATASET GENERATOR</h1>
     """)
 
     with gr.Row():
         with gr.Column():
-            gr.ChatInterface(chatbot=bot1, fn=gemini_api, title="Function Calling")
-            # create a Test Case with symbol and input
+            gr.ChatInterface(
+                fn=gemini_api,
+                chatbot=bot1,
+                title="Function Calling",
+            )
 
         with gr.Column():
-            gr.ChatInterface(chatbot=bot2, fn=gemini_api, title="Response")
+            gr.ChatInterface(
+                fn=gemini_api,
+                chatbot=bot2,
+                title="Response"
+            )
 
-    gr.Button("Save to Database")
+    # gr.Button("Save to Database")
 
 
 if __name__ == "__main__":
-    demo.launch()
+    iface.launch(server_name="0.0.0.0")
