b9fa49bb18975cb1669ea78757a89ef26f8bd061
Swarnim Burnwal <98446769+SLAYERDAKU@users.noreply.github.com>
2024-01-01 23:56:04 +0530

    Add semantic similarity and ngram comparison in web UI

--- experiments/datasets/symantic_similarity_webui.py
+++ experiments/datasets/symantic_similarity_webui.py
@@ -6,7 +6,11 @@ import os
 import gradio as gr
 from openai import OpenAI
 from scipy.spatial.distance import cosine
+from ngram import NGram
 from dotenv import load_dotenv
+from transformers import BertTokenizer, BertModel
+tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+model = BertModel.from_pretrained("bert-base-cased")
 
 load_dotenv("synthetic/.env")
 client = OpenAI(api_key=os.getenv("OPENAI_KEY_EMBEDINGS"))
@@ -21,7 +25,9 @@ def compare(text_1, text_2) -> float:
     text_1_embedding = get_embedding(text_1)
     text_2_embedding = get_embedding(text_2)
     similarity = 1 - cosine(text_1_embedding, text_2_embedding)
-    return round(similarity, 3)
+    ngram=NGram.compare(text_1, text_2, N=2)
+    # bert_embedding=get_embedding(text_1, model="text-embedding-bert-002")
+    return round(similarity, 3), round(ngram, 3)
 
 with gr.Blocks() as iface:
     gr.HTML("""
@@ -31,10 +37,15 @@ with gr.Blocks() as iface:
     with gr.Row():
         text_1 = gr.TextArea(lines=30, label="Text 1")
         text_2 = gr.TextArea(lines=30, label="Text 2")
+        
+    with gr.Row():
     
-    output = gr.Textbox(label="Semantic Similarity Score", interactive=False)
+        output_embedding = gr.Textbox(label="Semantic Similarity Score", interactive=False)
+        output_ngram= gr.Textbox(label="Ngram Similarity Score", interactive=False)
+        output_bert = gr.Textbox(label="BERT Similarity Score", interactive=False)
+
     compare_btn = gr.Button(value="Compare")
-    compare_btn.click(compare, inputs=[text_1, text_2], outputs=output)
+    compare_btn.click(compare, inputs=[text_1, text_2], outputs=[output_embedding, output_ngram])
     
 
 if __name__ == "__main__":
--- experiments/datasets/synthetic-mt/webui_calc.py
+++ experiments/datasets/synthetic-mt/webui_calc.py
@@ -2,22 +2,22 @@
 This file is used to generate mt data for JARVIS for `calculator`.
 """
 
-import streamlit as st
+# import streamlit as st
 from threading import Thread
 from queue import Queue
 import google.generativeai as genai
 import json
 
-st.set_page_config(
-    layout="wide",
-    page_title="ToolFormers (Calculator)",
-)
+# st.set_page_config(
+#     layout="wide",
+#     page_title="ToolFormers (Calculator)",
+# )
 data_points = Queue(maxsize=4)
-st.header("ToolFormers (Calculator)")
-st.session_state.data = None
+# st.header("ToolFormers (Calculator)")
+# st.session_state.data = None
 
 
-genai.configure(api_key="AIzaSyD2FkGlX8WQUt8JUXqW8Wp2OoENfgLynRQ")
+genai.configure(api_key="AIzaSyDtqp125rVbUDH-I3ooH7lcFabsa3fu0vI")
 
 # Set up the model
 generation_config = {
@@ -33,38 +33,83 @@ model = genai.GenerativeModel(
 prompt = [
     "{\"role\": \"system\", \"content\": \"Your task is to generate data to train a virtual assistant that has access to calculator. It can only perform the following operations: addition '+', subtraction '-', multiplication '*', division '/ ', power '**'). It can also use these operations to perform complicated computations. Don't include any other symbol not defined.\"},\n {\"user\": \"Jarvis, can you help me split the restaurant bill among three people? The total is $120.\", \"assistant\": \"the answer is <calculator> 2+2 </s> 4 </calculator>four. </s>\"},\n\t{\"user\": \"what is the square root of 4\", \"assistant\": \"the square root of 4 is <calculator> 4** 0.5 </s> 2. </s>\"},\n\t{\"user\": \"Jarvis, can you help me split the restaurant bill among three people? The total is 120 dollars.\", \"assistant\": \"Certainly, sir. Let me divide that for you. <calculator> 120 / 3 </s> 40 </calculator>Each person should contribute 40.</s> Anything else you'd like assistance with?\"},\n\t{\"user\": \"Hey Jarvis, I'm planning to buy a new phone. It costs 800 dolars, and the sales tax is 8 percent. Can you calculate the total amount I'll be paying?\", \"assistant\": \"Certainly, sir. Let me calculate that for you. <calculator> 800 + (800 * 0.08) </s> 864 </calculator>The total amount, including tax, is 864 dollars.</s> Is there anything else you'd like to know?\"},\n\t{\"user\": \"Jarvis, can you help me figure out my monthly expenses? I earn 3000 dollars, and I want to save 20 percent of my income.\", \"assistant\": \"Absolutely, sir. Let me calculate that for you. <calculator> 3000 * 0.20 </s> 600 </calculator>You should save 600 dollars each month.</s>\"},\nwrite more such user assistant data to train the assistant. queries shouldn't include any symbols beside + - * / .only generate one example at a time.\n"]
 
+print(prompt)
+
+# def push_data_to_queue(data_points: Queue):
+#     while True:
+#         response = model.generate_content(prompt)
+#         try:
+#             response = json.loads(response.text)
+#             data_points.put(response)
+#             print(data_points.qsize())
+#         except json.decoder.JSONDecodeError:
+#             return push_data_to_queue(data_points)
+        
+# push_data_to_queue(data_points)
+
 
 def push_data_to_queue(data_points: Queue):
     while True:
-        response = model.generate_content(prompt)
-        try:
-            response = json.loads(response.text)
-            data_points.put(response)
-            print(data_points.qsize())
-        except json.decoder.JSONDecodeError:
-            return push_data_to_queue(data_points)
+        if data_points.qsize() <= data_points.maxsize:
+            response = model.generate_content(prompt)
+            try:
+                response = json.loads(response.text)
+                data_points.put(response)
+                print(response)
+                print(data_points.qsize())
+
+            except json.decoder.JSONDecodeError:
+                return push_data_to_queue(data_points)
+           
+    
+        while not data_points.empty():
+            try:
+                data_point = data_points.get()
+                print(data_point)
+                print(data_points.qsize())
+                yield data_point
+            finally:
+                data_points.task_done()
 
 
-Thread(target=push_data_to_queue, args=(data_points,)).start()
+for i,j in enumerate(push_data_to_queue(data_points)):
+    print( "This is data",j)
 
 
-def write_data_point():
-    """Write a data point to the web UI"""
-    st.session_state.data = data_points.get()
-    data_point = st.session_state.data
-    st.markdown("### USER")
-    st.text(data_point["user"])
-    st.markdown("### JARVIS")
-    st.write(data_point["assistant"])
+# def extract_all_data_points(data_points: Queue):
+#     """Extract all data points from the queue"""
+#     while not data_points.empty():
+#         data_point = data_points.get()
+#         print(data_point)
+#         print(data_points.qsize())
+#         yield data_point
 
 
-ok_btn = st.button("OK", type="primary")
-reject_btn = st.button("Reject", type="secondary")
+# Thread(target=push_data_to_queue, args=(data_points,))
 
-if ok_btn or st.session_state.data is None:
-    print("Writing to Json file")
-    write_data_point()
 
-if reject_btn:
-    print("Rejecting")
-    write_data_point()
+
+# for i, j in enumerate(extract_all_data_points(data_points)):
+#         print(j)
+# def write_data_point():
+#     """Write a data point to the web UI"""
+
+    # st.session_state.data = data_point
+    # st.markdown("### USER")
+    # st.text(data_point["user"])
+    # st.markdown("### JARVIS")
+    # st.write(data_point["assistant"])
+
+
+# write_data_point()
+
+# ok_btn = st.button("OK", type="primary")
+# reject_btn = st.button("Reject", type="secondary")
+
+# if ok_btn or st.session_state.data is None:
+#     print("Writing to Json file")
+#     write_data_point()
+
+# if reject_btn:
+#     print("Rejecting")
+#     write_data_point()
