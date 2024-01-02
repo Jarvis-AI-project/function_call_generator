f6117ff528c586e6db83dc4cbcd47f3de237adee
Swarnim Burnwal <98446769+SLAYERDAKU@users.noreply.github.com>
2024-01-02 23:02:34 +0530

    Add platform-specific model path and tokenizer initialization

--- experiments/datasets/synthetic-mt/webui_calc.py
+++ experiments/datasets/synthetic-mt/webui_calc.py
@@ -12,11 +12,18 @@ import os
 from pymilvus import connections, Collection, db
 from dotenv import load_dotenv
 from transformers import AutoTokenizer, AutoModel
+import platform
 
-_MODEL_PATH = "/mnt/nvme/MODELS/EMBEDINGS/UAE-Large-V1/"
+if platform.system()=="Linux":
 
-tokenizer = AutoTokenizer.from_pretrained(_MODEL_PATH, device="cuda:1")
-bert_model = AutoModel.from_pretrained(_MODEL_PATH, device_map="cuda:1", low_cpu_mem_usage=True)
+    _MODEL_PATH = "/mnt/nvme/MODELS/EMBEDINGS/UAE-Large-V1/"
+    tokenizer = AutoTokenizer.from_pretrained(_MODEL_PATH, device="cuda:1")
+    bert_model = AutoModel.from_pretrained(_MODEL_PATH, device_map="cuda:1", low_cpu_mem_usage=True)
+
+else:
+    tokenizer = AutoTokenizer.from_pretrained("WhereIsAI/UAE-Large-V1")
+    bert_model = AutoModel.from_pretrained("WhereIsAI/UAE-Large-V1")
+
 
 data_points = Queue(maxsize=4)
 
@@ -26,8 +33,8 @@ genai.configure(api_key="AIzaSyDtqp125rVbUDH-I3ooH7lcFabsa3fu0vI")
 
 connections.connect(
     alias="default",
-    host = os.getenv("MILVUS_HOST"),
-    port = 19530,
+    host=os.getenv("MILVUS_HOST"),
+    port=19530,
     user=os.getenv("MILVUS_USER"),
     password=os.getenv("MILVUS_PASSWORD"),
 )
@@ -49,11 +56,18 @@ model = genai.GenerativeModel(
 prompt = """
 I want some data to train my language model. The model can only perform simple calculations including plus, minus, multiply, divide and power. the operations are represented by the signs + - * / **. don't include any other signs in the data.
 
-Example:
+Example 1:
 USER: can you split a bill for me
 ASSISTANT: Of course! I'd be happy to help you split a bill. Please provide me with the total amount of the bill and the number of people you want to split it among. </s>
 USER: the total amount is five hundred dollars and split it between five people
 ASSISTANT: Each person should contribute <calculator> 500/5 </s> 100 </calculator> hundred dollars.</s>
+
+Example 2:
+USER: I need to find the total cost of coffee and cake.
+ASSISTANT: Please provide me with the cost of coffee and cake separately. </s>
+USER: the coffee costs three dollars and cake costs five dollars.
+ASSISTANT: The total cost for coffee and cake together is <calculator> 3+5 </s> 8 </calculator> eight dollars. </s>
+
 
 Generate more such conversations to train the assistant. 
 - The main calculation should be surrounded by <calculator> and </calculator> which will not be shown to the user and is evaluated by a computer. After the assistant stops talking write this </s>
@@ -63,9 +77,13 @@ Generate more such conversations to train the assistant.
 - Assume this system is being used to do day-to-day tasks.
 - Do not explain your calculation.
 - Avoid generating data points that require up-to-date knowledge.
-- User query can't contain symbols such as (%, $), etc. Instead, it should use the corresponding word representation of the symbols eg. dollar for $ and percentage for %
-- If the user query contains a number or is in numerical format, convert it into words eg. 100 -> hundred or $500 -> five hundred dollars.
+- User query can't contain any kind symbols such as (%, $,₹, .,), etc. Instead, it should use the corresponding word representation of the symbols eg. dollar for $ and percentage for %
+- If the user query contains currency or a number, convert it into words eg. ₹ 100 -> one Hundred rupees , 3500 rupees -> thirty-five hundred rupees, ₹ 500 -> five hundred rupees , 0.5 -> or 5 -> five  .
+- Always use currency as rupees 
+- the user query should not contain </s> eg. Calculate the cost of a dozen eggs for me.
+- the assistant response should contain  </s> after numbers  in between the opening and closing tags of <calculator> and </calculator> and also after end of the response. eg. ASSISTANT: The cost of a dozen eggs would be <calculator> 1*12 </s> 12 </calculator> twelve rupees. </s>
 - Output only one conversation at a time. 
+
 """
 
 
@@ -81,10 +99,11 @@ def push_data_to_queue(data_points: Queue):
         else:
             sleep(1)
 
+
 def get_to_3_similar_conversations(current_conversation):
     # Get the top 3 conversations from the database symanitcally similar to the current conversation
     results = collection.search(
-        data = [get_embedings(current_conversation)],
+        data=[get_embedings(current_conversation)],
         anns_field="embeding",
         param={"metric_type": "COSINE"},
         limit=3,
@@ -93,26 +112,30 @@ def get_to_3_similar_conversations(current_conversation):
 
     prev_conversations = []
     for hit in results:
-        prev_conversations.append(f"Similarity: {hit.distance}\n" + hit.entity.get('conversation').get('data'))
+        prev_conversations.append(
+            f"Similarity: {hit.distance}\n" + hit.entity.get('conversation').get('data'))
 
     if len(prev_conversations) < 3:
-        prev_conversations.extend(["Not Enough Data"] * (3 - len(prev_conversations)))
+        prev_conversations.extend(
+            ["Not Enough Data"] * (3 - len(prev_conversations)))
 
     return prev_conversations
-    
+
 
 def get_embedings(conv: str):
-    tokens = tokenizer(conv, return_tensors="pt", padding=True, truncation=True)
+    tokens = tokenizer(conv, return_tensors="pt",
+                       padding=True, truncation=True)
     tokens = {k: v.to(bert_model.device) for k, v in tokens.items()}
     with torch.no_grad():
         embedings = bert_model(**tokens).last_hidden_state[:, -1]
     return embedings.squeeze().cpu().numpy().tolist()
 
+
 def accept_data(current_conversation):
     if not current_conversation:
         gr.Error("No data to accept")
         return
-    
+
     # Insert current conversation into the database
     collection.insert(
         [{
@@ -127,10 +150,12 @@ def accept_data(current_conversation):
     similar_conversations = get_to_3_similar_conversations(new_conversation)
     return new_conversation, similar_conversations[0], similar_conversations[1], similar_conversations[2]
 
+
 def reject_data():
     new_conversation = data_points.get()
     similar_conversations = get_to_3_similar_conversations(new_conversation)
     return new_conversation, similar_conversations[0], similar_conversations[1], similar_conversations[2]
+
 
 with gr.Blocks(theme=gr.themes.Soft()) as demo:
     gr.HTML("""
@@ -138,7 +163,8 @@ with gr.Blocks(theme=gr.themes.Soft()) as demo:
     """)
 
     with gr.Row():
-        mt_conversation = gr.Textbox(label="Generated MT Conversation", placeholder="Click Reject to get 1st data point", interactive=True)
+        mt_conversation = gr.Textbox(label="Generated MT Conversation",
+                                     placeholder="Click Reject to get 1st data point", interactive=True)
 
     with gr.Row():
         accept_btn = gr.Button(value="Accept")
@@ -146,13 +172,18 @@ with gr.Blocks(theme=gr.themes.Soft()) as demo:
 
     with gr.Column():
         gr.Markdown("## Top Conversation taht already exists in the database")
-        prev_conversation_1 = gr.Textbox(label="Conversation 1", placeholder="", interactive=False)
-        prev_conversation_2 = gr.Textbox(label="Conversation 2", placeholder="", interactive=False)
-        prev_conversation_3 = gr.Textbox(label="Conversation 3", placeholder="", interactive=False)
+        prev_conversation_1 = gr.Textbox(
+            label="Conversation 1", placeholder="", interactive=False)
+        prev_conversation_2 = gr.Textbox(
+            label="Conversation 2", placeholder="", interactive=False)
+        prev_conversation_3 = gr.Textbox(
+            label="Conversation 3", placeholder="", interactive=False)
 
-    accept_btn.click(accept_data, inputs=[mt_conversation], outputs=[mt_conversation, prev_conversation_1, prev_conversation_2, prev_conversation_3])
-    reject_btn.click(reject_data, outputs=[mt_conversation, prev_conversation_1, prev_conversation_2, prev_conversation_3])
-    
+    accept_btn.click(accept_data, inputs=[mt_conversation], outputs=[
+                     mt_conversation, prev_conversation_1, prev_conversation_2, prev_conversation_3])
+    reject_btn.click(reject_data, outputs=[
+                     mt_conversation, prev_conversation_1, prev_conversation_2, prev_conversation_3])
+
 if __name__ == "__main__":
     Thread(target=push_data_to_queue, args=(data_points,)).start()
     demo.launch(share=True)
