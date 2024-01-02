f5c9130ee05f1b765fdf990481fe85658f88f3bb
Devasheesh Mishra <devasheeshmishra4@gmail.com>
2024-01-02 12:04:01 +0000

    Update webui_calc.py with conversation acceptance/rejection functionality

--- experiments/datasets/synthetic-mt/webui_calc.py
+++ experiments/datasets/synthetic-mt/webui_calc.py
@@ -4,25 +4,37 @@ This file is used to generate mt data for JARVIS for `calculator`.
 
 from threading import Thread
 from queue import Queue
+import torch
 import google.generativeai as genai
-import json
+# import json
 from time import sleep
 import gradio as gr
-from ngram import NGram
-from sklearn.metrics.pairwise import cosine_similarity
+import os
+from pymilvus import connections, Collection
+# from ngram import NGram
+# from sklearn.metrics.pairwise import cosine_similarity
+from dotenv import load_dotenv
 from transformers import AutoTokenizer, AutoModel
 
-tokenizer = AutoTokenizer.from_pretrained(
-    "WhereIsAI/UAE-Large-V1", device="mps")
-bert_model = AutoModel.from_pretrained("WhereIsAI/UAE-Large-V1",
-                                       device_map="mps")
+_MODEL_PATH = "/mnt/nvme/MODELS/EMBEDINGS/UAE-Large-V1/"
 
+tokenizer = AutoTokenizer.from_pretrained(_MODEL_PATH, device="cuda:1")
+bert_model = AutoModel.from_pretrained(_MODEL_PATH, device_map="cuda:1", low_cpu_mem_usage=True)
 
 data_points = Queue(maxsize=4)
 
+load_dotenv(".env")
 
 genai.configure(api_key="AIzaSyDtqp125rVbUDH-I3ooH7lcFabsa3fu0vI")
 
+connections.connect(
+    host = os.getenv("MILVUS_HOST"),
+    port = 19530,
+    user=os.getenv("MILVUS_USER"),
+    password=os.getenv("MILVUS_PASSWORD"),
+)
+
+collection = Collection("JARVIS_calculator", using="default")
 
 generation_config = {
     "temperature": 0.9,
@@ -36,7 +48,6 @@ model = genai.GenerativeModel(
 )
 
 prompt = """
-
 I want some data to train my language model. The model can only perform simple calculations including plus, minus, multiply, divide and power. the operations are represented by the signs + - * / **. don't include any other signs in the data.
 
 Example:
@@ -54,7 +65,6 @@ Generate more such conversations to train the assistant.
 - Do not explain your calculation.
 - Avoid generating data points that require up-to-date knowledge.
 - Only output one conversation at a time. 
-
 """
 
 
@@ -63,10 +73,7 @@ def push_data_to_queue(data_points: Queue):
         if not data_points.full():
             response = model.generate_content(prompt)
             try:
-                response = response.text
-                print(response)
-                print(data_points.qsize())
-                data_points.put(response)
+                data_points.put(response.text)
             except Exception as e:
                 gr.Error(e)
                 pass
@@ -74,32 +81,83 @@ def push_data_to_queue(data_points: Queue):
             sleep(1)
 
 
-def get_ngram(user, assistant):
-    """Ngram Similarity of two text chunks."""
-    return round(NGram.compare(user, assistant, N=2), 2)
+# def get_ngram(user, assistant):
+#     """Ngram Similarity of two text chunks."""
+#     return round(NGram.compare(user, assistant, N=2), 2)
 
 
-def get_bert_embedding(user, assistant):
-    """BERT Similarity of two text chunks."""
-    user = tokenizer(user, return_tensors="pt", padding=True, truncation=True)
-    user = {k: v.to(bert_model.device) for k, v in user.items()}
-    assistant = tokenizer(assistant, return_tensors="pt",
-                          padding=True, truncation=True)
-    assistant = {k: v.to(bert_model.device) for k, v in assistant.items()}
+# def get_bert_embedding(user, assistant):
+#     """BERT Similarity of two text chunks."""
+#     user = tokenizer(user, return_tensors="pt", padding=True, truncation=True)
+#     user = {k: v.to(bert_model.device) for k, v in user.items()}
+#     assistant = tokenizer(assistant, return_tensors="pt",
+#                           padding=True, truncation=True)
+#     assistant = {k: v.to(bert_model.device) for k, v in assistant.items()}
 
-    user_embeddings = bert_model(**user).last_hidden_state[:, -1]
-    assistant_embeddings = bert_model(**assistant).last_hidden_state[:, -1]
-    return round(cosine_similarity(user_embeddings.detach().cpu().numpy(), assistant_embeddings.detach().cpu().numpy())[0][0], 5)
+#     user_embeddings = bert_model(**user).last_hidden_state[:, -1]
+#     assistant_embeddings = bert_model(**assistant).last_hidden_state[:, -1]
+#     return round(cosine_similarity(user_embeddings.detach().cpu().numpy(), assistant_embeddings.detach().cpu().numpy())[0][0], 5)
 
 
-def extract_and_compare():
-    data = data_points.get()
-    user = data
-    assistant = data
-    similarity = get_bert_embedding(user, assistant)
-    output_ngram = get_ngram(user, assistant)
-    return user, assistant, similarity, output_ngram
+# def extract_and_compare():
+#     data = data_points.get()
+#     user = data
+#     assistant = data
+#     similarity = get_bert_embedding(user, assistant)
+#     output_ngram = get_ngram(user, assistant)
+#     return user, assistant, similarity, output_ngram
 
+def get_new_conversation():
+    new_conversation = data_points.get()
+    return new_conversation
+
+def get_to_3_similar_conversations(current_conversation):
+    # Get the top 3 conversations from the database symanitcally similar to the current conversation
+    results = collection.search(
+        data = [get_embedings(current_conversation)],
+        anns_field="Embedings",
+        param={"metric_type": "L2"},
+        limit=3,
+        output_fields=["Conversation"]
+    )[0]
+
+    prev_conversations = []
+    for hit in results:
+        # prev_conversations.update({"Conversation": hit.entity.get('Conversation'), "Similarity": hit.distance})
+        prev_conversations.append(f"Similarity: {hit.distance}\n" + hit.entity.get('Conversation').get('data'))
+
+    if len(prev_conversations) < 3:
+        prev_conversations.extend(["Not Enough Data"] * (3 - len(prev_conversations)))
+
+    return prev_conversations
+    
+
+def get_embedings(conv: str):
+    return torch.randn(1024).numpy().tolist()
+
+def accept_data(current_conversation):
+    if not current_conversation:
+        gr.Error("No data to accept")
+        return
+    
+    # Insert current conversation into the database
+    collection.insert(
+        {
+            "Embedings": get_embedings(current_conversation),
+            "Conversation": {
+                "data": current_conversation,
+            }
+        }
+    )
+
+    new_conversation = get_new_conversation()
+    similar_conversations = get_to_3_similar_conversations(new_conversation)
+    return new_conversation, similar_conversations[0], similar_conversations[1], similar_conversations[2]
+
+def reject_data():
+    new_conversation = get_new_conversation()
+    similar_conversations = get_to_3_similar_conversations(new_conversation)
+    return new_conversation, similar_conversations[0], similar_conversations[1], similar_conversations[2]
 
 with gr.Blocks(theme=gr.themes.Soft()) as demo:
     gr.HTML("""
@@ -107,24 +165,21 @@ with gr.Blocks(theme=gr.themes.Soft()) as demo:
     """)
 
     with gr.Row():
+        mt_conversation = gr.TextArea(label="Generated MT Conversation", placeholder="Click Reject to get 1st data point", interactive=True)
 
-       data= gr.Textbox(label="Data", lines=20, placeholder="Data will appear here", interactive=True)
     with gr.Row():
-
-        output_ngram = gr.Textbox(
-            label="Ngram Similarity Score", interactive=False)
-        output_bert = gr.Textbox(
-            label="BERT Similarity Score", interactive=False)
-    with gr.Row():
-
         accept_btn = gr.Button(value="Accept")
         reject_btn = gr.Button(value="Reject")
-        accept_btn.click(
-            extract_and_compare, outputs=[data,output_ngram, output_bert])
 
+    with gr.Column():
+        gr.Markdown("## Top Conversation taht already exists in the database")
+        prev_conversation_1 = gr.Textbox(label="Conversation 1", placeholder="", interactive=False)
+        prev_conversation_2 = gr.Textbox(label="Conversation 2", placeholder="", interactive=False)
+        prev_conversation_3 = gr.Textbox(label="Conversation 3", placeholder="", interactive=False)
 
+    accept_btn.click(accept_data, inputs=[mt_conversation], outputs=[mt_conversation, prev_conversation_1, prev_conversation_2, prev_conversation_3])
+    reject_btn.click(reject_data, outputs=[mt_conversation, prev_conversation_1, prev_conversation_2, prev_conversation_3])
+    
 if __name__ == "__main__":
-
     Thread(target=push_data_to_queue, args=(data_points,)).start()
-
     demo.launch()
